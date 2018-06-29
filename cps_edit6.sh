a50de19e8666a606d809058cf7f17ae911eac82d
chris adamson <chris.adamson@mcri.edu.au>
2018-06-29 11:35:34 +1000

    Changed file names

--- cps_edit6.sh
+++ cps_edit6.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+T=`tmpnam`
+
 if [ -d "$1" ]
 then
 	
@@ -11,15 +13,22 @@ then
 		LHWHITESURF=$SUBJECTS_DIR/$1/surf/lh.white
 		RHWHITESURF=$SUBJECTS_DIR/$1/surf/rh.white
 	fi
-	freeview -v \
-	 $SUBJECTS_DIR/$1/mri/T1.mgz \
-	 $SUBJECTS_DIR/$1/mri/brainmask.mgz:grayscale=31,103 \
-	 $SUBJECTS_DIR/$1/mri/wm.mgz:visible=1:colormap=gecolor:opacity=0.5 \
-	 -f $LHWHITESURF:edgecolor=blue:edgethickness=3 \
-	 $SUBJECTS_DIR/$1/surf/lh.pial:edgecolor=red \
-	 $RHWHITESURF:edgecolor=blue:edgethickness=3 \
-	 $SUBJECTS_DIR/$1/surf/rh.pial:edgecolor=red \
-	 -c $SUBJECTS_DIR/$1/tmp/control.dat:radius=1
+	cat << EOF > $T
+	-v $SUBJECTS_DIR/$1/mri/T1.mgz
+	-v $SUBJECTS_DIR/$1/mri/brainmask.mgz:grayscale=31,103
+	-v $SUBJECTS_DIR/$1/mri/wm.mgz:visible=1:colormap=gecolor:opacity=0.5
+	-v $SUBJECTS_DIR/$1/mri/surface.defects.mgz:colormap=lut:lut=$FREESURFER_HOME/DefectLUT.txt:opacity=0.35
+	-f $LHWHITESURF:edgecolor=blue:edgethickness=3
+	-f $SUBJECTS_DIR/$1/surf/lh.pial:edgecolor=red
+	-f $RHWHITESURF:edgecolor=blue:edgethickness=3
+	-f $SUBJECTS_DIR/$1/surf/rh.pial:edgecolor=red
+EOF
+	if [ -f "$SUBJECTS_DIR/$1/tmp/control.dat" ]
+	then
+		echo "-c $SUBJECTS_DIR/$1/tmp/control.dat:radius=1" >> $T
+	fi
+	freeview -cmd $T
+	rm -f $T
 	 
 #SUBJECTS_DIR/$1/mri/aseg.presurf.mgz:colormap=lut:opacity=0.35 \
 #$SUBJECTS_DIR/$1/mri/aseg.mask.mgz:colormap=lut:opacity=0.35 \
